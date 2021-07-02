32e5b96064faf0eecc8b7057d5d9248ca74cfd40
Gourimath, Sagar <sagar.gourimath@gmail.com>
2021-07-02 08:42:36 +0530

    Enabling the submit button.

--- Udemy - React Front To Back/github-finder/src/App.js
+++ Udemy - React Front To Back/github-finder/src/App.js
@@ -11,22 +11,22 @@ class App extends Component {
     loading: false,
   };
 
-  async componentDidMount() {
+  searchGitUsers = async (text) => {
     this.setState({ loading: true });
 
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ loading: false, users: res.data });
-  }
+    this.setState({ loading: false, users: res.data.items });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar title="GitHub Finder" icon="fab fa-github" />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchGitUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
